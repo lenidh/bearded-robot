71ea7955d4d9dfb06b1f0d0a57674d6945ccff68
Moritz Heindl <lenidh@gmail.com>
2014-04-04 18:32:15 +0200

    DynamicRuntime-Erweiterung und BIOS-Klasse
    - Restliche DynamicRuntime-Methodenimplementierungen hinzugefügt
    - BIOS-Klasse hinzugefügt.

--- src/rte/DynamicRuntime.java
+++ src/rte/DynamicRuntime.java
@@ -3,7 +3,7 @@ package rte;
 /**
  * Stellt verschiedene Methoden bereit, die für die Laufzeitumgebung nötig sind.
  */
-@SuppressWarnings({"UnusedDeclaration", "SpellCheckingInspection"})
+@SuppressWarnings({"UnusedDeclaration", "SpellCheckingInspection", "JavaDoc"}) // TODO: JavaDoc
 public class DynamicRuntime {
 
 	/**
@@ -22,18 +22,17 @@ public class DynamicRuntime {
 
 	/**
 	 * Wird aufgerufen, wenn zur Laufzeit ein neues Objekt erzeugt werden soll.
-	 *
 	 * <p>
-	 * Diese Methode wird aufgerufen, wenn das new Schlüsselwort zur erzeugung
+	 * Diese Methode wird aufgerufen, wenn das new Schlüsselwort zur Erzeugung
 	 * von Objekten verwendet wird. Deshalb sollte diese Methode niemals direkt
 	 * gerufen werden.
 	 * </p>
 	 *
-	 * @param scalarSize Die Summe der Größen aller Basisdatentyp-Attribute,
-	 *                   welche der Objekttyp definiert.
+	 * @param scalarSize   Die Summe der Größen aller Basisdatentyp-Attribute,
+	 *                     welche der Objekttyp definiert.
 	 * @param relocEntries Die Anzahl der Referenz-Attribute, welche der
 	 *                     Objekttyp definiert.
-	 * @param type Der Klassendiskriptor des Objekttyps.
+	 * @param type         Der Klassendiskriptor des Objekttyps.
 	 * @return Eine neues Objekt.
 	 */
 	public static Object newInstance(int scalarSize, int relocEntries, SClassDesc type) {
@@ -51,7 +50,7 @@ public class DynamicRuntime {
 		while (objSize % 4 != 0) objSize++;
 
 		// Initialisiere Speicher mit 0.
-		for(int i = newInstanceOffset; i < (objSize / 4); i++) {
+		for (int i = newInstanceOffset; i < (objSize / 4); i++) {
 			MAGIC.wMem32(i, 0);
 		}
 
@@ -74,36 +73,199 @@ public class DynamicRuntime {
 		return obj;
 	}
 
+	/**
+	 * Wird aufgerufen, wenn zur Laufzeit ein neues Array erzeugt werden soll.
+	 * <p>
+	 * Diese Methode wird aufgerufen, wenn das new Schlüsselwort zur Erzeugung
+	 * von Arrays verwendet wird. Deshalb sollte diese Methode niemals direkt
+	 * gerufen werden.
+	 * </p>
+	 *
+	 * @param length
+	 * @param arrDim
+	 * @param entrySize
+	 * @param stdType
+	 * @param unitType
+	 * @return Eine neues Array.
+	 */
 	public static SArray newArray(int length, int arrDim, int entrySize, int stdType,
 	                              Object unitType) {
-		//noinspection InfiniteLoopStatement,StatementWithEmptyBody
-		while(true);
+		int scS, rlE;
+		SArray me;
+
+		if (stdType == 0 && unitType._r_type != MAGIC.clssDesc("SClassDesc"))
+			MAGIC.inline(0xCC); //check type of unitType, we don't support interface arrays
+		scS = MAGIC.getInstScalarSize("SArray");
+		rlE = MAGIC.getInstRelocEntries("SArray");
+		if (arrDim > 1 || entrySize < 0) rlE += length;
+		else scS += length * entrySize;
+		me = (SArray)newInstance(scS, rlE, (SClassDesc)MAGIC.clssDesc("SArray"));
+		MAGIC.assign(me.length, length);
+		MAGIC.assign(me._r_dim, arrDim);
+		MAGIC.assign(me._r_stdType, stdType);
+		MAGIC.assign(me._r_unitType, unitType);
+		return me;
 	}
 
+	/**
+	 * Wird aufgerufen, wenn zur Laufzeit ein neues mehrdimensionales Array
+	 * erzeugt werden soll.
+	 * <p>
+	 * Diese Methode wird aufgerufen, wenn das new Schlüsselwort zur Erzeugung
+	 * von mehrdimensionalen Arrays verwendet wird. Deshalb sollte diese Methode
+	 * niemals direkt gerufen werden.
+	 * </p>
+	 *
+	 * @param length
+	 * @param arrDim
+	 * @param entrySize
+	 * @param stdType
+	 * @param clssType
+	 */
 	public static void newMultArray(SArray[] parent, int curLevel, int destLevel, int length,
-	                                int arrDim, int entrySize, int stdType, Object unitType) {
-		//noinspection InfiniteLoopStatement,StatementWithEmptyBody
-		while(true);
+	                                int arrDim, int entrySize, int stdType, SClassDesc clssType) {
+		int i;
+
+		if (curLevel + 1 < destLevel) { //step down one level
+			curLevel++;
+			for (i = 0; i < parent.length; i++) {
+				newMultArray((SArray[])((Object)parent[i]), curLevel, destLevel,
+						length, arrDim, entrySize, stdType, clssType);
+			}
+		} else { //create the new entries
+			destLevel = arrDim - curLevel;
+			for (i = 0; i < parent.length; i++) {
+				parent[i] = newArray(length, destLevel, entrySize, stdType, clssType);
+			}
+		}
 	}
 
-	public static boolean isInstance(Object o, SClassDesc dest, boolean asCast) {
-		//noinspection InfiniteLoopStatement,StatementWithEmptyBody
-		while(true);
+	/**
+	 * Wird aufgerufen, wenn zur Laufzeit die Objekt-Klassen-Hierarchie geprüft
+	 * wird.
+	 * <p>
+	 * Diese Methode wird aufgerufen, wenn das instanceof Schlüsselwort zur
+	 * Prüfung der Objekt-Klassen-Hierarchie verwendet wird. Deshalb sollte
+	 * diese Methode niemals direkt gerufen werden.
+	 * </p>
+	 *
+	 * @param o
+	 * @param dest
+	 * @param asCast
+	 * @return true falls der Typ des Objekts von der Klasse abgeleitet ist,
+	 * sonst false.
+	 */
+	public static boolean isInstance(Object o, Object dest, boolean asCast) {
+		SClassDesc check;
+
+		if (o == null) {
+			return asCast; // true: null matches all; false: null is not an instance
+		}
+		check = o._r_type;
+		while (check != null) {
+			if (check == dest) return true;
+			check = check.parent;
+		}
+		if (asCast) MAGIC.inline(0xCC);
+		return false;
 	}
 
+	/**
+	 * Wird aufgerufen, wenn zur Laufzeit die Implementierung eines Interfaces
+	 * geprüft wird.
+	 * <p>
+	 * Diese Methode wird aufgerufen, wenn das instanceof Schlüsselwort zur
+	 * Prüfung auf Interfaceimplementierungen genutzt wird. Deshalb sollte diese
+	 * Methode niemals direkt gerufen werden.
+	 * </p>
+	 *
+	 * @param o
+	 * @param dest
+	 * @param asCast
+	 * @return true falls der Typ des Objekts das Interface implementiert, sonst
+	 * false.
+	 */
 	public static SIntfMap isImplementation(Object o, SIntfDesc dest, boolean asCast) {
-		//noinspection InfiniteLoopStatement,StatementWithEmptyBody
-		while(true);
+		SIntfMap check;
+
+		if (o == null) return null;
+		check = o._r_type.implementations;
+		while (check != null) {
+			if (check.owner == dest) return check;
+			check = check.next;
+		}
+		if (asCast) MAGIC.inline(0xCC);
+		return null;
 	}
 
-	public static boolean isArray(SArray o, int stdType, Object unitType, int arrDim,
+	/**
+	 * Wird aufgerufen, wenn zur Laufzeit den Typ eines Arrays.
+	 * <p>
+	 * Diese Methode wird aufgerufen, wenn das instanceof Schlüsselwort zur
+	 * Prüfung auf Arraytypen genutzt wird. Deshalb sollte diese Methode niemals
+	 * direkt gerufen werden.
+	 * </p>
+	 *
+	 * @param o
+	 * @param stdType
+	 * @param clssType
+	 * @param arrDim
+	 * @param asCast
+	 * @return true falls das Array vom gesuchten Typ ist, sonst false.
+	 */
+	public static boolean isArray(SArray o, int stdType, Object clssType, int arrDim,
 	                              boolean asCast) {
-		//noinspection InfiniteLoopStatement,StatementWithEmptyBody
-		while(true);
+		SClassDesc clss;
+
+		//in fact o is of type "Object", _r_type has to be checked below - but this check is faster than "instanceof" and conversion
+		if (o == null) {
+			return asCast; // true: null matches all; false: null is not an instance
+		}
+		if (o._r_type != MAGIC.clssDesc("SArray")) { //will never match independently of arrDim
+			if (asCast) MAGIC.inline(0xCC);
+			return false;
+		}
+		if (clssType == MAGIC.clssDesc("SArray")) { //special test for arrays
+			if (o._r_unitType == MAGIC.clssDesc("SArray"))
+				arrDim--; //an array of SArrays, make next test to ">=" instead of ">"
+			if (o._r_dim > arrDim)
+				return true; //at least one level has to be left to have an object of type SArray
+			if (asCast) MAGIC.inline(0xCC);
+			return false;
+		}
+		//no specials, check arrDim and check for standard type
+		if (o._r_stdType != stdType || o._r_dim < arrDim) { //check standard types and array dimension
+			if (asCast) MAGIC.inline(0xCC);
+			return false;
+		}
+		//noinspection ConstantConditions
+		if (stdType != 0) {
+			if (o._r_dim == arrDim) return true; //array of standard-type matching
+			if (asCast) MAGIC.inline(0xCC);
+			return false;
+		}
+		//array of objects, make deep-check for class type (PicOS does not support interface arrays)
+		if (o._r_unitType._r_type != MAGIC.clssDesc("SClassDesc")) MAGIC.inline(0xCC);
+		clss = (SClassDesc)o._r_unitType;
+		while (clss != null) {
+			if (clss == clssType) return true;
+			clss = clss.parent;
+		}
+		if (asCast) MAGIC.inline(0xCC);
+		return false;
 	}
 
-	public static void checkArrayStore(Object dest, SArray newEntry) {
-		//noinspection InfiniteLoopStatement,StatementWithEmptyBody
-		while(true);
+	/**
+	 * Prüft beim Speichern von Elementen in einem Array, ob die Array- und
+	 * Elementtypen zueinander passen.
+	 *
+	 * @param dest
+	 * @param newEntry
+	 */
+	public static void checkArrayStore(SArray dest, SArray newEntry) {
+		if (dest._r_dim > 1)
+			isArray(newEntry, dest._r_stdType, dest._r_unitType, dest._r_dim - 1, true);
+		else if (dest._r_unitType == null) MAGIC.inline(0xCC);
+		else isInstance(newEntry, dest._r_unitType, true);
 	}
 }
